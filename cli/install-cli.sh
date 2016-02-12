0922ef0c0e637a8d020f30b04c8af297502719e0
Scott Warner <scott.warner@nextcentury.com>
2016-02-11 22:02:54 -0500

    Update the CLI install script

--- cli/install-cli.sh
+++ cli/install-cli.sh
@@ -10,26 +10,32 @@ fi
 
 echo "Installing to $INSTALL_BASE"
 
+JAR_PATH="$INSTALL_BASE/jars/stix"
+
+if [ -d "$JAR_PATH" ]; then
+    echo "Existing jar path found, cleaning"
+    rm -f "$JAR_PATH/*"
+fi
+
 # Copy all the jars and properties into the jars folder
-mkdir -p "$INSTALL_BASE/jars/stix"
+mkdir -p "$JAR_PATH"
 
-cp target/*.jar "$INSTALL_BASE/jars/stix"
+cp target/*.jar "$JAR_PATH"
 # Copy any properties, ignoring errors
-cp target/*.properties "$INSTALL_BASE/jars/stix" 2> /dev/null || :
+cp target/*.properties "$JAR_PATH" 2> /dev/null || :
 
-cp *.properties "$INSTALL_BASE/jars/stix" 2> /dev/null || :
+cp *.properties "$JAR_PATH" 2> /dev/null || :
 
 # Copy the scripts into the bin folder
 mkdir -p "$INSTALL_BASE/bin"
 cp stix.* "$INSTALL_BASE/bin"
 
 # Set permissions
-chmod -R a+r "$INSTALL_BASE/jars/stix"
-chmod -R a-w "$INSTALL_BASE/jars/stix"
+chmod -R 0775 "$JAR_PATH"
 
 # Make sure the directories have +x
 chmod a+x "$INSTALL_BASE/bin"
-chmod a+x "$INSTALL_BASE/jars/stix"
+chmod a+x "$JAR_PATH"
 # Make sure the scripts are executable
 for f in `ls $INSTALL_BASE/bin/stix.*`; do
     chmod a+x "$f"
